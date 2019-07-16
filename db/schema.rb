66f56d037756efe03a0c8a66be89ba91cdc0dbaf
soledad escobar <m.sol.escobar@gmail.com>
2019-07-16 14:45:03 -0300

    table add photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_191622) do
+ActiveRecord::Schema.define(version: 2019_07_16_170304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_191622) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
@@ -35,6 +36,15 @@ ActiveRecord::Schema.define(version: 2019_07_15_191622) do
     t.datetime "updated_at", null: false
     t.index ["animal_id"], name: "index_bookings_on_animal_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +62,5 @@ ActiveRecord::Schema.define(version: 2019_07_15_191622) do
   add_foreign_key "animals", "users"
   add_foreign_key "bookings", "animals"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
 end
