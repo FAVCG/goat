455589b424931a3a51c65ae1fa7e880ab8c7df4d
luis zhang <luischn94@gmail.com>
2019-07-19 17:00:53 -0300

    new seeds with accounts

--- app/javascript/plugins/flatpicker.js
+++ app/javascript/plugins/flatpicker.js
@@ -1,4 +1,36 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
+import flatpickr from 'flatpickr';
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 flatpickr(".datepicker", {})
+
+// const toggleDateInputs = function() {
+//   const startDateInput = document.getElementById('booking_start_date');
+//   const endDateInput = document.getElementById('booking_end_date');
+
+//   if (startDateInput && endDateInput) {
+//     const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable)
+
+//     flatpickr(startDateInput, {
+//     minDate: 'today',
+//     dateFormat: 'd-m-Y',
+//     disable: unvailableDates,
+//     onChange: function(selectedDates, selectedDate) {
+//       if (selectedDate === '') {
+//         endDateInput.disabled = true;
+//       }
+//       let minDate = selectedDates[0];
+//       minDate.setDate(minDate.getDate() + 1);
+//       endDateCalendar.set('minDate', minDate);
+//       endDateInput.disabled = false;
+//     }
+//   });
+//     const endDateCalendar =
+//       flatpickr(endDateInput, {
+//         dateFormat: 'd-m-Y',
+//         disable: unvailableDates,
+//         },
+//       });
+//   }
+// };
+
+// export { toggleDateInputs }
