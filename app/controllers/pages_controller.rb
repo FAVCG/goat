04b3a207fef5632e5e1a519caba7a17e34f2a035
luis zhang <luischn94@gmail.com>
2019-07-19 11:10:13 -0300

    authenticate user fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def home
+  end
+  def index
   end
 end
